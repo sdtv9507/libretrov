2237230f051ff2d9741a899d7c6bb1e858838b3c
sdtv9507 <apr8950@gmail.com>
2021-10-08 18:50:02 -0400

    Add public access to structs

--- libretro_structs.v
+++ libretro_structs.v
@@ -1,12 +1,13 @@
 module libretrov
 
-struct C.retro_vfs_file_handle {
+pub struct C.retro_vfs_file_handle {
 }
 
-struct C.retro_vfs_dir_handle {
+pub struct C.retro_vfs_dir_handle {
 }
 
-struct C.retro_vfs_interface {
+pub struct C.retro_vfs_interface {
+pub:
 	// VFS API v1
 	get_path Retro_vfs_get_path_t
 	open     Retro_vfs_open_t
@@ -31,7 +32,8 @@ struct C.retro_vfs_interface {
 	closedir        Retro_vfs_closedir_t
 }
 
-struct C.retro_vfs_interface_info {
+pub struct C.retro_vfs_interface_info {
+pub:
 	required_interface_version u32
 	/*
 	Frontend writes interface pointer here. The frontend also sets the actual
@@ -41,16 +43,19 @@ struct C.retro_vfs_interface_info {
 	iface &C.retro_vfs_interface
 }
 
-struct C.retro_hw_render_interface {
+pub struct C.retro_hw_render_interface {
+pub:
 	interface_type    Retro_hw_render_interface_type
 	interface_version u32
 }
 
-struct C.retro_led_interface {
+pub struct C.retro_led_interface {
+pub:
 	set_led_state Retro_set_led_state_t
 }
 
-struct C.retro_midi_interface {
+pub struct C.retro_midi_interface {
+pub:
 	input_enabled  Retro_midi_input_enabled_t
 	output_enabled Retro_midi_output_enabled_t
 	read           Retro_midi_read_t
@@ -58,12 +63,14 @@ struct C.retro_midi_interface {
 	flush          Retro_midi_flush_t
 }
 
-struct C.retro_hw_render_context_negotiation_interface {
+pub struct C.retro_hw_render_context_negotiation_interface {
+pub:
 	interface_type    Retro_hw_render_context_negotiation_interface_type
 	interface_version u32
 }
 
-struct C.retro_memory_descriptor {
+pub struct C.retro_memory_descriptor {
+pub mut:
 	flags      u64
 	ptr        voidptr
 	offset     usize
@@ -74,27 +81,32 @@ struct C.retro_memory_descriptor {
 	addrspace  &char
 }
 
-struct C.retro_memory_map {
+pub struct C.retro_memory_map {
+pub:
 	descriptors     &C.retro_memory_descriptor
 	num_descriptors u32
 }
 
-struct C.retro_controller_description {
+pub struct C.retro_controller_description {
+pub:
 	desc &char
 	id   u32
 }
 
-struct C.retro_controller_info {
+pub struct C.retro_controller_info {
+pub:
 	types     &C.retro_controller_description
 	num_types u32
 }
 
-struct C.retro_subsystem_memory_info {
+pub struct C.retro_subsystem_memory_info {
+pub:
 	extension &char
 	c_type    u32
 }
 
-struct C.retro_subsystem_rom_info {
+pub struct C.retro_subsystem_rom_info {
+pub:
 	desc             &char
 	valid_extensions &char
 	need_fullpath    bool
@@ -104,7 +116,8 @@ struct C.retro_subsystem_rom_info {
 	num_memory       u32
 }
 
-struct C.retro_subsystem_info {
+pub struct C.retro_subsystem_info {
+pub:
 	desc     &char
 	ident    &char
 	roms     C.retro_subsystem_rom_info
@@ -112,15 +125,18 @@ struct C.retro_subsystem_info {
 	id       u32
 }
 
-struct C.retro_get_proc_address_interface {
+pub struct C.retro_get_proc_address_interface {
+pub:
 	get_proc_address Retro_get_proc_address_t
 }
 
-struct C.retro_log_callback {
+pub struct C.retro_log_callback {
+pub:
 	log Retro_log_printf_t
 }
 
-struct C.retro_perf_counter {
+pub struct C.retro_perf_counter {
+pub:
 	ident      &char
 	start      Retro_perf_tick_t
 	total      Retro_perf_tick_t
@@ -128,7 +144,8 @@ struct C.retro_perf_counter {
 	registered bool
 }
 
-struct C.retro_perf_callback {
+pub struct C.retro_perf_callback {
+pub:
 	get_time_usec    Retro_perf_get_time_usec_t
 	get_cpu_features Retro_get_cpu_features_t
 
@@ -139,12 +156,14 @@ struct C.retro_perf_callback {
 	perf_log         Retro_perf_log_t
 }
 
-struct C.retro_sensor_interface {
+pub struct C.retro_sensor_interface {
+pub:
 	set_sensor_state Retro_set_sensor_state_t
 	get_sensor_input Retro_sensor_get_input_t
 }
 
-struct C.retro_camera_callback {
+pub struct C.retro_camera_callback {
+pub:
 	caps                  u64
 	width                 u32
 	height                u32
@@ -156,7 +175,8 @@ struct C.retro_camera_callback {
 	deinitialized         Retro_camera_lifetime_status_t
 }
 
-struct C.retro_location_callback {
+pub struct C.retro_location_callback {
+pub:
 	start        Retro_location_start_t
 	stop         Retro_location_stop_t
 	get_position Retro_location_get_position_t
@@ -166,26 +186,31 @@ struct C.retro_location_callback {
 	deinitialized Retro_location_lifetime_status_t
 }
 
-struct C.retro_rumble_interface {
+pub struct C.retro_rumble_interface {
+pub:
 	set_rumble_state Retro_set_rumble_state_t
 }
 
-struct C.retro_audio_callback {
+pub struct C.retro_audio_callback {
+pub:
 	callback  Retro_audio_callback_t
 	set_state Retro_audio_set_state_callback_t
 }
 
-struct C.retro_frame_time_callback {
+pub struct C.retro_frame_time_callback {
+pub:
 	callback Retro_frame_time_callback_t
 
 	reference Retro_usec_t
 }
 
-struct C.retro_audio_buffer_status_callback {
+pub struct C.retro_audio_buffer_status_callback {
+pub:
 	callback Retro_audio_buffer_status_callback_t
 }
 
-struct C.retro_hw_render_callback {
+pub struct C.retro_hw_render_callback {
+pub:
 	context_type            Retro_hw_context_type
 	context_reset           Retro_hw_context_reset_t
 	get_current_framebuffer Retro_hw_get_current_framebuffer_t
@@ -200,11 +225,13 @@ struct C.retro_hw_render_callback {
 	debug_context           bool
 }
 
-struct C.retro_keyboard_callback {
+pub struct C.retro_keyboard_callback {
+pub:
 	callback Retro_keyboard_event_t
 }
 
-struct C.retro_disk_control_callback {
+pub struct C.retro_disk_control_callback {
+pub:
 	set_eject_state Retro_set_eject_state_t
 	get_eject_state Retro_get_eject_state_t
 
@@ -216,7 +243,8 @@ struct C.retro_disk_control_callback {
 	add_image_index     Retro_add_image_index_t
 }
 
-struct C.retro_disk_control_ext_callback {
+pub struct C.retro_disk_control_ext_callback {
+pub:
 	set_eject_state Retro_set_eject_state_t
 	get_eject_state Retro_get_eject_state_t
 
@@ -233,12 +261,14 @@ struct C.retro_disk_control_ext_callback {
 	get_image_label Retro_get_image_label_t
 }
 
-struct C.retro_message {
+pub struct C.retro_message {
+pub:
 	msg    &char
 	frames u32
 }
 
-struct C.retro_message_ext {
+pub struct C.retro_message_ext {
+pub:
 	msg      &char
 	duration u32
 	priority u32
@@ -248,7 +278,8 @@ struct C.retro_message_ext {
 	progress i8
 }
 
-struct C.retro_input_descriptor {
+pub struct C.retro_input_descriptor {
+pub:
 	port   u32
 	device u32
 	index  u32
@@ -257,8 +288,8 @@ struct C.retro_input_descriptor {
 	description &char
 }
 
-struct C.retro_system_info {
-mut:
+pub struct C.retro_system_info {
+pub mut:
 	library_name    &char
 	library_version &char
 
@@ -267,13 +298,15 @@ mut:
 	block_extract    bool
 }
 
-struct C.retro_system_content_info_override {
+pub struct C.retro_system_content_info_override {
+pub:
 	extensions      &char
 	need_fullpath   bool
 	persistent_data bool
 }
 
-struct C.retro_game_info_ext {
+pub struct C.retro_game_info_ext {
+pub:
 	full_path       &char
 	archive_path    &char
 	archive_file    &char
@@ -287,8 +320,8 @@ struct C.retro_game_info_ext {
 	persistent_data bool
 }
 
-struct C.retro_game_geometry {
-mut:
+pub struct C.retro_game_geometry {
+pub mut:
 	base_width  u32
 	base_height u32
 	max_width   u32
@@ -297,36 +330,40 @@ mut:
 	aspect_ratio f32
 }
 
-struct C.retro_system_timing {
-mut:
+pub struct C.retro_system_timing {
+pub mut:
 	fps         f64
 	sample_rate f64
 }
 
-struct C.retro_system_av_info {
-mut:
+pub struct C.retro_system_av_info {
+pub mut:
 	geometry C.retro_game_geometry
 	timing   C.retro_system_timing
 }
 
-struct C.retro_variable {
+pub struct C.retro_variable {
+pub:
 	key &char
 
 	value &char
 }
 
-struct C.retro_core_option_display {
+pub struct C.retro_core_option_display {
+pub:
 	key     &char
 	visible bool
 }
 
-struct C.retro_core_option_value {
+pub struct C.retro_core_option_value {
+pub:
 	value &char
 
 	label &char
 }
 
-struct C.retro_core_option_definition {
+pub struct C.retro_core_option_definition {
+pub:
 	key &char
 
 	desc &char
@@ -338,19 +375,22 @@ struct C.retro_core_option_definition {
 	default_value &char
 }
 
-struct C.retro_core_options_intl {
+pub struct C.retro_core_options_intl {
+pub:
 	us &C.retro_core_option_definition
 
 	local &C.retro_core_option_definition
 }
 
-struct C.retro_core_option_v2_category {
+pub struct C.retro_core_option_v2_category {
+pub:
 	key  &char
 	desc &char
 	info &char
 }
 
-struct C.retro_core_option_v2_definition {
+pub struct C.retro_core_option_v2_definition {
+pub:
 	key              &char
 	desc             &char
 	desc_categorized &char
@@ -361,28 +401,33 @@ struct C.retro_core_option_v2_definition {
 	default_value    &char
 }
 
-struct C.retro_core_options_v2 {
+pub struct C.retro_core_options_v2 {
+pub:
 	categories  &C.retro_core_option_v2_category
 	definitions &C.retro_core_option_v2_definition
 }
 
-struct C.retro_core_options_v2_intl {
+pub struct C.retro_core_options_v2_intl {
+pub:
 	us    &C.retro_core_options_v2
 	local &C.retro_core_options_v2
 }
 
-struct C.retro_core_options_update_display_callback {
+pub struct C.retro_core_options_update_display_callback {
+pub:
 	callback Retro_core_options_update_display_callback_t
 }
 
-struct C.retro_game_info {
+pub struct C.retro_game_info {
+pub:
 	path &char
 	data voidptr
 	size usize
 	meta &char
 }
 
-struct C.retro_framebuffer {
+pub struct C.retro_framebuffer {
+pub mut:
 	data         voidptr
 	width        u32
 	height       u32
@@ -392,7 +437,8 @@ struct C.retro_framebuffer {
 	memory_flags u32
 }
 
-struct C.retro_fastforwarding_override {
+pub struct C.retro_fastforwarding_override {
+pub:
 	ratio          f32
 	fastforward    bool
 	notification   bool
